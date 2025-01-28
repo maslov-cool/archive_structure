c6ddb30ef02986b32719a0d9f99bf4a20d2a62fd
alex <106116143+maslov-cool@users.noreply.github.com>
2025-01-28 17:30:01 +0300

    Добавление размера файлов к структуре

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
 from zipfile import ZipFile
 
 
-with ZipFile(input('введите архив')) as myzip:
+def human_read_format(size):
+    sizes = ['Б', 'КБ', 'МБ', 'ГБ']
+    n = size
+    i = 0
+    while n // 1024:
+        i += 1
+        n //= 1024
+    return str(round(size / 1024 ** i)) + sizes[i]
+
+
+with ZipFile('input.zip') as myzip:
     info = myzip.infolist()
 for i in info:
     if i.orig_filename[-1] == '/':
         print((i.orig_filename.count('/') - 1) * '  ' + i.orig_filename.split('/')[-2])
     else:
-        print(i.orig_filename.count('/') * '  ' + i.orig_filename.split('/')[-1])
+        print(i.orig_filename.count('/') * '  ' + i.orig_filename.split('/')[-1] + ' ' +
+              str(human_read_format(i.file_size)))
+
